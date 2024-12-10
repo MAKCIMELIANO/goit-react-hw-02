1e338e9247d75b686831cd4593d696be29e4a081
MAKCIMELIANO <makcimeliano@gmail.com>
2024-12-10 05:31:49 +0200

    edit

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,3 +1,4 @@
+import { FaSmile, FaMeh, FaFrown } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import css from './Feedback.module.css';
@@ -6,11 +7,18 @@ export default function Feedback({ good, neutral, bad, total, positive }) {
   return (
     <div className={css.feedback}>
       <ul className={css.feedbackList}>
-        <li className={clsx(css.feedbackItem, css.good)}>Good: {good}</li>
+        <li className={clsx(css.feedbackItem, css.good)}>
+          Good: {good}
+          <FaSmile className={clsx(css.icon, css.good)} />
+        </li>
         <li className={clsx(css.feedbackItem, css.neutral)}>
           Neutral: {neutral}
+          <FaMeh className={clsx(css.icon, css.neutral)} />
         </li>
-        <li className={clsx(css.feedbackItem, css.bad)}>Bad: {bad}</li>
+        <li className={clsx(css.feedbackItem, css.bad)}>
+          Bad: {bad}
+          <FaFrown className={clsx(css.icon, css.bad)} />
+        </li>
         <li className={css.feedbackItem}>Total: {total}</li>
         <li className={css.feedbackItem}>Positive: {positive}%</li>
       </ul>
